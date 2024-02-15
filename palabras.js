a3ba5804b5b585d5d7b7d5aeb46af3c43ef2fe83
Cecilia Gonzalez <155860795+ceciliagonzalez7@users.noreply.github.com>
2024-02-15 19:09:42 -0300

    Add files via upload

---
+++ palabras.js
@@ -0,0 +1,2 @@
+const palabras = ['martillo', 'carne', 'lavarropas', 'sucio', 'corazon', 'lentes', 'juego', 'cereza', 'jugadores', 'equipo', 'computadora', 'gato', 'perro', 'sentir', 'aventura', 'cangrejo', 'azucar', 'zanahoria', 'amor', 'bandera', 'viento', 'dorado', 'felicidad', 'ñandu']
+
---
+++ script.js
@@ -0,0 +1,114 @@
+const contenedorPalabra = document.getElementById('contenedorPalabra');
+const startButton = document.getElementById('startButton');
+const letrasUsadasElement = document.getElementById('letrasUsadas');
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+ctx.canvas.width = 0;
+ctx.canvas.height = 0;
+
+const bodyParts = [
+    [4, 2, 1, 1],
+    [4, 3, 1, 2],
+    [3, 5, 1, 1],
+    [5, 5, 1, 1],
+    [3, 3, 1, 1],
+    [5, 3, 1, 1],
+];
+
+let selectedWord;
+let letrasUsadas;
+let errores;
+let hits;
+
+const addLetter = letter  => {
+    const letterElement = document.createElement('span');
+    letterElement.innerHTML = letter.toUpperCase();
+    letrasUsadasElement.appendChild(letterElement);
+};
+
+const addBodyPart = bodyParts => {
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(...bodyParts);
+};
+
+const wrongLetter = ()  => {
+    addBodyPart(bodyParts[errores]);
+    errores++;
+    if (errores === bodyParts.length) endGame;
+};
+
+const endGame = ()  => {
+    document.removeEventListener('keydown', letterEvent);
+    startButton.style.display = 'block';
+};
+
+const correctLetter = letter  => {
+    const { children } = contenedorPalabra;
+    for(let i = 0; i < children.length; i++) {
+        if(children[i].innerHTML === letter) {
+            children[i].classList.toggle('hidden');
+            hits++;
+        }
+    }
+    if (hits === selectedWord.length) endGame(); 
+};
+
+const letterInput = letter  => {
+    if (selectedWord.includes(letter)) {
+        correctLetter(letter);
+    } else {
+        wrongLetter();
+    }
+    addLetter(letter);
+    letrasUsadas.push(letter);
+};
+
+const letterEvent = event => {
+    let newLetter = event.key.toUpperCase();
+    if(newLetter.match(/^[a-zñ]$/i) && !letrasUsadas.includes(newLetter)) {
+        letterInput(newLetter);
+    };
+};
+
+const drawWord = () =>{
+    selectedWord.forEach(letter => {
+        const letterElement = document.createElement('span');
+        letterElement.innerHTML = letter.toUpperCase();
+        letterElement.classList.add('letter');
+        letterElement.classList.add('hidden');
+        contenedorPalabra.appendChild(letterElement);
+    });
+}
+
+const selectRandomWord = () =>{
+    let palabra = palabras[Math.floor((Math.random() * palabras.length))].toUpperCase();
+    selectedWord = palabra.split('');
+};
+
+const drawHangMan = () =>{
+    ctx.canvas.width = 120;
+    ctx.canvas.height = 160;
+    ctx.scale(20, 20);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#d95d39';
+    ctx.fillRect(0, 7, 4, 1);
+    ctx.fillRect(1, 0, 1, 8);
+    ctx.fillRect(2, 0, 3, 1);
+    ctx.fillRect(4, 1, 1, 1);
+};
+
+const startGame = () => {
+    letrasUsadas = [];
+    errores = 0;
+    hits = 0;
+    contenedorPalabra.innerHTML = '';
+    letrasUsadasElement.innerHTML = '';
+    startButton.style.display = 'none';
+    drawHangMan();
+    selectRandomWord();
+    drawWord();
+    document.addEventListener('keydown', letterEvent);
+};
+
+startButton.addEventListener('click', startGame);
